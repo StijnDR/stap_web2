a37dc0077492b185664bfb97b4082a66bab19cba
Stijn <stijn_de_raeve@hotmail.com>
2014-04-01 12:31:33 +0200

    Register

--- MvcStap/Models/Bedrijf.cs
+++ MvcStap/Models/Bedrijf.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -46,5 +47,61 @@ namespace MvcStap.Models
             this.wachtwoordContact = wachtwoordC;
         }
         #endregion
+
+        public class RegisterModel
+        {
+           
+            
+
+            // Bedrijf gegevens
+            [Required]
+            [Display(Name = "Bedrijfsnaam")]
+            public string naamBedrijf { get; set; }
+
+            [Required]
+            [Display(Name = "Adres")]
+            public String adresBedrijf;
+
+            [Required]
+            [Display(Name = "Gemeente")]
+            public String gemeenteBedrijf;
+
+            [Required]
+            [Display(Name = "Website")]
+            public String urlBedrijf;
+
+            [Required]
+            [Display(Name = "Bereikbaarheid")]
+            public String bereikbaarheidBedrijf;
+
+            [Required]
+            [Display(Name = "Omschrijving")]
+            public String omschrijvingBedrijf;
+
+            // Contact gegevens 
+            [Required]
+            [Display(Name = "Naam contactpersoon")]
+            public String naamContact;
+
+            [Required]
+            [Display(Name = "Email contactpersoon")]
+            public String emailContact;
+
+            [Required]
+            [Display(Name = "Telefoon contactpersoon")]
+            public String telefoonContact;
+
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string wachtwoordContact { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmWachtwoordContact { get; set; }
+        }
     }
 }
